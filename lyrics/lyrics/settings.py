e2cd4950f27238e26be68a1b558f8586a9e2e2cc
zbrianhuang <67428806+zbrianhuang@users.noreply.github.com>
2025-05-01 18:58:36 +0800

    maybe

--- lyrics/lyrics/settings.py
+++ lyrics/lyrics/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-7ecrpv%gqxqh17$)vf1swd(q+cr$r1r4g0qc=3&vtrpki0g8z1"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["typing-game-mh4i.onrender.com","localhost"]
 
