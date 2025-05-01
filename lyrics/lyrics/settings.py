128d1dd6acc99e078f3b151b1063de3d63489a11
zbrianhuang <67428806+zbrianhuang@users.noreply.github.com>
2025-05-01 19:04:31 +0800

    buh

--- lyrics/lyrics/settings.py
+++ lyrics/lyrics/settings.py
@@ -116,7 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "/typing-game/static/"
+STATIC_URL = "/lyrics/typing-game/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
